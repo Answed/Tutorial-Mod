4a6cbea2e5da175ad3aafccf12e12d9442a1ea5b
Answed <schradearwed@gmail.com>
2023-07-21 14:21:50 +0200

    Sapphire ore now drops raw sapphire

--- src/main/java/de/answed/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/de/answed/tutorialmod/item/ModCreativeModTabs.java
@@ -21,6 +21,7 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
                     })
                     .build());
 
