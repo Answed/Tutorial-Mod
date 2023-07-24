ddc0fe55828267bddc2131b1d95b5c6f352ea67d
Answed <schradearwed@gmail.com>
2023-07-24 12:12:42 +0200

    Added Logic to ModBlockStateProvider

--- src/main/java/de/answed/tutorialmod/datagen/ModBlockStateProvider.java
+++ src/main/java/de/answed/tutorialmod/datagen/ModBlockStateProvider.java
@@ -1,4 +1,26 @@
 package de.answed.tutorialmod.datagen;
 
-public class ModBlockStateProvider {
+import de.answed.tutorialmod.TutorialMod;
+import de.answed.tutorialmod.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper){
+        super(output, TutorialMod.MODID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels(){
+        blockWithItem(ModBlocks.SAPPHIRE_ORE);
+        blockWithItem(ModBlocks.SAPPHIRE_BLOCK);
+        blockWithItem(ModBlocks.RAW_SAPPHIRE_BLOCK);
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject){
+        simpleBlockItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
 }
