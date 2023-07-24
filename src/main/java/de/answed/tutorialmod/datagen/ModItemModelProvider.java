15a3d2c768b6639cd7191f6a5d47fc1fd17482e8
Answed <schradearwed@gmail.com>
2023-07-24 12:18:52 +0200

    Added Logic to ModItemModelProvider

--- src/main/java/de/answed/tutorialmod/datagen/ModItemModelProvider.java
+++ src/main/java/de/answed/tutorialmod/datagen/ModItemModelProvider.java
@@ -1,4 +1,37 @@
 package de.answed.tutorialmod.datagen;
 
-public class ModItemModelProvider {
+import de.answed.tutorialmod.TutorialMod;
+import de.answed.tutorialmod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, TutorialMod.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.RAW_SAPPHIRE);
+        simpleItem(ModItems.SAPPHIRE);
+        simpleItem(ModItems.METAL_DETECTOR);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item){
+        return withExistingParent(item.getId().getPath(),
+            new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(TutorialMod.MODID, "item"+item.getId().getPath()));
+    }
+
+    private ItemModelBuilder handheldItem(RegistryObject<Item> item){
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/handheld")).texture("layer0",
+                new ResourceLocation(TutorialMod.MODID, "item"+item.getId().getPath()));
+    }
+
 }
