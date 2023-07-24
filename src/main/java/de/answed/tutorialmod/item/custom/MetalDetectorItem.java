06233ab40955eee751e2c740ae38d26eb3f91c60
Answed <schradearwed@gmail.com>
2023-07-24 11:43:06 +0200

    Added use On and isValuable Block

--- src/main/java/de/answed/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/de/answed/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,9 +1,41 @@
 package de.answed.tutorialmod.item.custom;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class MetalDetectorItem extends Item{
     public MetalDetectorItem(Properties pPropertries){
         super(pPropertries);
     }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        if(pContext.getLevel().isClientSide()){
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player player = pContext.getPlayer();
+            boolean foundBlock = false;
+
+            for(int i = 0; i <= positionClicked.getY() + 64; i++){
+                BlockState state = pContext.getLevel().getBlockState(positionClicked.below(i));
+
+                if(isValuableBlock(state)){
+                    outputValuableCoordinates(positionClicked.below(i), player, state.getBlock());
+                    foundBlock = true;
+                    break;
+                }
+            }
+        }
+        return super.useOn(pContext);
+    }
+
+    private boolean isValuableBlock(BlockState state) {
+        return state.is(Blocks.IRON_ORE) || state.is(Blocks.GOLD_ORE);
+    }
+
+
 }
