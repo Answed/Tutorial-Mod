9d67a924f4bb0ab29245f1907d71a023b14bea77
Answed <schradearwed@gmail.com>
2023-07-24 11:36:41 +0200

    Added MetalDetecor class

---
+++ src/main/java/de/answed/tutorialmod/item/custom/MetalDetectorItem.java
@@ -0,0 +1,9 @@
+package de.answed.tutorialmod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class MetalDetectorItem extends Item{
+    public MetalDetectorItem(Properties pPropertries){
+        super(pPropertries);
+    }
+}
