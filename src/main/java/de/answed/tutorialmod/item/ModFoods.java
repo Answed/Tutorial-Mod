6e25efe2d004e351cf6a1487d09a7502c12f877c
Answed <schradearwed@gmail.com>
2023-07-28 07:31:00 +0200

    Added Strawberry logic

---
+++ src/main/java/de/answed/tutorialmod/item/ModFoods.java
@@ -0,0 +1,10 @@
+package de.answed.tutorialmod.item;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(2)
+            .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 30), 0.5f).build();
+}
