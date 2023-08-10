279f6627ea80c829bb46302958701d2a8f3470af
Answed <schradearwed@gmail.com>
2023-08-10 17:19:57 +0200

    Added ModLootTable Provider

--- src/main/java/de/answed/tutorialmod/datagen/ModItemTagGenerator.java
+++ src/main/java/de/answed/tutorialmod/datagen/ModItemTagGenerator.java
@@ -17,6 +17,6 @@ public class ModItemTagGenerator extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
-        
+
     }
 }
--- src/main/java/de/answed/tutorialmod/datagen/ModLootTableProvider.java
+++ src/main/java/de/answed/tutorialmod/datagen/ModLootTableProvider.java
@@ -1,15 +1,17 @@
 package de.answed.tutorialmod.datagen;
 
+import de.answed.tutorialmod.datagen.loot.ModBlockLootTables;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 
 import java.util.List;
 import java.util.Set;
 
 public class ModLootTableProvider {
     public static LootTableProvider create(PackOutput output){
-        return new LootTableProvider(output, Set.of(),
-                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSet.builder().build())));
+        return new LootTableProvider(output, Set.of(), List.of(
+                new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)
+        ));
     }
 }
